e3d84383b1f7e871274e5672dd9fcbedbadc60ac
Jared Wray <me@jaredwray.com>
2025-05-04 12:09:54 -0700

    testing paths

--- test/static.test.ts
+++ test/static.test.ts
@@ -1,10 +1,26 @@
+import path from 'node:path';
 import {describe, test, expect} from 'vitest';
 import fastify from 'fastify';
-import {StaticPathOptions, registerStatic} from '../src/static.js';
+import {type StaticPathOptions, registerStatic} from '../src/static.js';
 
 describe('Static', async () => {
 	test('should export all modules', async () => {
 		expect(registerStatic).toBeDefined();
 		expect(registerStatic).toBeTypeOf('function');
+	});
+	test('should register static paths', async () => {
+		const app = fastify();
+		const options: StaticPathOptions = [
+			{
+				dir: 'test/fixtures',
+				path: '/static',
+			},
+			{
+				dir: path.resolve('test1/fixtures'),
+				path: '/static1',
+			},
+		];
+		await registerStatic(app, options);
+		expect(app).toBeDefined();
 	});
 });
