17568551b6fa80ffeb42f9c3007488895edffed1
Jared Wray <me@jaredwray.com>
2025-06-18 12:21:09 -0700

    feat: adding in OpenAPI spec and docs

---
+++ test/open-api.test.ts
@@ -0,0 +1,94 @@
+import {describe, test, expect} from 'vitest';
+import Fastify from 'fastify';
+import {
+	fuse, start, type StartOptions, type FuseOptions,
+} from '../src/index.js';
+import {type OpenApiOptions} from '../src/open-api.js';
+
+describe('Open API', async () => {
+	test('should be able to fuse with OpenAPI', async () => {
+		// eslint-disable-next-line new-cap
+		const app = Fastify();
+		const options: FuseOptions = {
+			static: true,
+			openApi: true,
+		};
+		await fuse(app, options);
+		expect(app).toBeDefined();
+		expect(app).toBeTypeOf('object');
+		expect(app.server).toBeDefined();
+		expect(app.server).toBeTypeOf('object');
+	});
+
+	test('should be able to fuse with OpenAPI', async () => {
+		// eslint-disable-next-line new-cap
+		const app = Fastify();
+		const options: FuseOptions = {
+			static: true,
+			openApi: {
+				docsRoutePath: '/docs',
+			},
+		};
+		await fuse(app, options);
+		expect(app).toBeDefined();
+		expect(app).toBeTypeOf('object');
+		expect(app.server).toBeDefined();
+		expect(app.server).toBeTypeOf('object');
+	});
+
+	test('should be able to access the OpenAPI defaults', async () => {
+		// eslint-disable-next-line new-cap
+		const fastify = Fastify();
+		const options: FuseOptions = {
+			static: true,
+			openApi: true,
+		};
+		await fuse(fastify, options);
+		const response = await fastify.inject({
+			method: 'GET',
+			url: '/',
+		});
+
+		const openApiResponse = await fastify.inject({
+			method: 'GET',
+			url: '/openapi/json',
+		});
+
+		expect(response.statusCode).toBe(200);
+		expect(response.headers['content-type']).toBe('text/html; charset=utf-8');
+		expect(response.body).toContain('data-url="/openapi/json"');
+
+		expect(openApiResponse.statusCode).toBe(200);
+		expect(openApiResponse.headers['content-type']).toBe('application/json; charset=utf-8');
+		expect(openApiResponse.body).toContain('"openapi":');
+	});
+
+	test('should be able to access the OpenAPI docs', async () => {
+		// eslint-disable-next-line new-cap
+		const fastify = Fastify();
+		const options: FuseOptions = {
+			static: true,
+			openApi: {
+				docsRoutePath: '/docs',
+			},
+		};
+		await fuse(fastify, options);
+		const response = await fastify.inject({
+			method: 'GET',
+			url: '/docs',
+		});
+
+		const openApiResponse = await fastify.inject({
+			method: 'GET',
+			url: '/openapi/json',
+		});
+
+		expect(response.statusCode).toBe(200);
+		expect(response.headers['content-type']).toBe('text/html; charset=utf-8');
+		expect(response.body).toContain('data-url="/openapi/json"');
+
+		expect(openApiResponse.statusCode).toBe(200);
+		expect(openApiResponse.headers['content-type']).toBe('application/json; charset=utf-8');
+		expect(openApiResponse.body).toContain('"openapi":');
+	});
+});
